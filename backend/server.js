71f37d0ddc4833c54f1ac41825414100852d2623
shivamg829 <shivamgangwar2197@gmail.com>
2025-11-22 22:58:00 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,16 @@ import taskRouter from './routes/taskRoutes.js';
 const app = express();
 
 const port = process.env.PORT || 5000;
+app.use(cors({
+  origin: [
+    'https://dayline-task.netlify.app', 
+    'http://localhost:5173'
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}))
 
-app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
